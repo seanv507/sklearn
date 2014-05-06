da74dac2f287466870739cb8a9806d12475347c7
sean <sean.violante@gmail.com>
2014-05-07 01:32:52 +0200

    cleaned up code - class learns for nsamples at time

--- source/mnist.py
+++ source/mnist.py
@@ -12,106 +12,143 @@ from sklearn.cross_validation import train_test_split
 from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler
 
-# label
+def load_data():
+    # label
 
-#[offset] [type]          [value]          [description] 
-#0000     32 bit integer  0x00000801(2049) magic number (MSB first) 
-#0004     32 bit integer  60000            number of items 
-#0008     unsigned byte   ??               label 
-#0009     unsigned byte   ??               label 
-#........ 
-#xxxx     unsigned byte   ??               label
+    #[offset] [type]          [value]          [description]
+    #0000     32 bit integer  0x00000801(2049) magic number (MSB first)
+    #0004     32 bit integer  60000            number of items
+    #0008     unsigned byte   ??               label
+    #0009     unsigned byte   ??               label
+    #........
+    #xxxx     unsigned byte   ??               label
+
+    with open('../data/train-labels-idx1-ubyte','rb') as f:
+        magic = np.fromfile(f,'>u4',1)[0]
+        nlabels = np.fromfile(f,'>u4',1)[0]
+        labels = np.fromfile(f,np.ubyte)
+    print magic, nlabels
+    
+    
+    #TRAINING SET IMAGE FILE (train-images-idx3-ubyte):
+    #
+    #[offset] [type]          [value]          [description]
+    #0000     32 bit integer  0x00000803(2051) magic number
+    #0004     32 bit integer  60000            number of images
+    #0008     32 bit integer  28               number of rows
+    #0012     32 bit integer  28               number of columns
+    #0016     unsigned byte   ??               pixel
+    #0017     unsigned byte   ??               pixel
+    #........
+    #xxxx     unsigned byte   ??               pixel
+    # Pixels are organized row-wise. Pixel values are 0 to 255.
+    # 0 means background (white), 255 means foreground (black).
+    
+    with open('../data/train-images-idx3-ubyte', 'rb') as f:
+        magic = np.fromfile(f, '>u4', 1)[0]
+        nimages = np.fromfile(f, '>u4', 1)[0]
+        nrows = np.fromfile(f, '>u4', 1)[0]
+        ncols = np.fromfile(f, '>u4', 1)[0]
+        images = np.fromfile(f, np.ubyte).reshape((nimages, nrows*ncols))
+    print magic, nimages, nrows, ncols
+    return images,labels,nimages,nrows,ncols
+
+class SGDLearner():
+
+    def __init__(self, X_train, y_train, X_test, y_test,
+                 random_state, eta0, alpha, T, update_period):
+        self.eta0 = eta0
+        self.alpha = alpha
+
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.rng = random_state
+        #E_part = np.NaN((self.T // self.update_period, ))
+
+        self.ntrain, self.ncoef = self.X_train.shape
+
+        self.wts = np.zeros((self.ntrain + 1, self.ncoef + 1))
+        self.grad = np.zeros((self.ntrain + 1, self.ncoef + 1))
+        self.reset()
+
+    def reset(self):
+        
+        self.scores=[]
+        
+        #turn into dataframe?
+        self.mn_grad = []
+        self.st_grad = []
+
+        self._iT = 0
+        self.sgd = SGDClassifier(warm_start=True,
+                                 random_state=self.rng, fit_intercept=True)
+        self.sgd.loss = "hinge"
+        self.sgd.alpha = self.alpha
+        self.sgd.learning_rate = 'constant'
+        self.sgd.eta0 = self.eta0
+
+    def learn(self, nsamples):
+        '''Train for nsamples and store results'''
+        ind = self.rng.randint(0, self.ntrain, nsamples)
+        self.sgd.partial_fit(self.X_train[ind, :], self.y_train[ind], [0, 1])
+        self._iT += nsamples
+
+        self.calc_grad()
+        mn = self.grad.mean(axis=0) * 1e-6
+        st = self.grad.std(axis=0)*1e-6
+
+        self.mn_grad.append(mn)
+        self.mn_grad.append(st)
+
+        ind = self.rng.randint(0, self.ntrain, int(self.ntrain*0.1))
+        scores = {'timestep': self._iT}
+        scores['part'] = \
+            self.sgd.score(self.X_train[ind, :], self.y_train[ind])
+        scores['train'] = \
+            self.sgd.score(self.X_train, self.y_train)
+        scores['test'] = \
+            self.sgd.score(self.X_test, self.y_test)
+        self.scores.append(scores)
+
+    def calc_grad(self):
+        # now calculate current gradient variance
+        # by using very low learning rate and calculate gradients
+        eta0 = self.sgd.eta0
+        self.sgd.eta0 = 1e-6*eta0
+        self.wts[0, :-1] = self.sgd.coef_
+        self.wts[0, -1] = self.sgd.intercept_
+        for im in range(self.ntrain):
+
+            self.sgd.partial_fit(self.X_train[im, :].reshape((1, -1)),
+                                 self.y_train[im].reshape((1, )), [0, 1])
+            self.wts[im, :-1] = self.sgd.coef_
+            self.wts[im, -1] = self.sgd.intercept_
+
+        self.grad = np.diff(self.wts, axis=0)
+        self.sgd.eta0 = eta0
+        # restore orignal learning rate
 
 
-#with open('train-labels-idx1-ubyte','rb') as f:
-#    magic=np.fromfile(f,'>u4',1)[0]
-#    nlabels=np.fromfile(f,'>u4',1)[0]
-#    labels=np.fromfile(f,np.ubyte)
-#print magic,nlabels
-#    
-
-#TRAINING SET IMAGE FILE (train-images-idx3-ubyte):
+#labels8=labels==8
 #
-#[offset] [type]          [value]          [description] 
-#0000     32 bit integer  0x00000803(2051) magic number 
-#0004     32 bit integer  60000            number of images 
-#0008     32 bit integer  28               number of rows 
-#0012     32 bit integer  28               number of columns 
-#0016     unsigned byte   ??               pixel 
-#0017     unsigned byte   ??               pixel 
-#........ 
-#xxxx     unsigned byte   ??               pixel
-# Pixels are organized row-wise. Pixel values are 0 to 255.
-# 0 means background (white), 255 means foreground (black).
-
-#with open('train-images-idx3-ubyte','rb') as f:
-#    magic=np.fromfile(f,'>u4',1)[0]
-#    nimages=np.fromfile(f,'>u4',1)[0]
-#    nrows=np.fromfile(f,'>u4',1)[0]
-#    ncols=np.fromfile(f,'>u4',1)[0]
-#    images=np.fromfile(f,np.ubyte).reshape((nimages,nrows*ncols))
-#print magic,nimages,nrows,ncols
-
-labels9=labels==9
-
-# split into train and test set
-
-images_all=np.zeros((nimages,nrows*ncols))
-images_all[:]=images[:]
-labels_all=labels9
-
-
-# split into train and test set
-rng=np.random.RandomState(2)
-images_train,images_test, labels_train,labels_test = train_test_split(images_all, labels_all,test_size=0.33, random_state=rng)
-ntrain=images_train.shape[0]
-
-images_train_mean=images_train.mean()
-images_train_std=images_train.std()
-
-# rescale
-scaler=StandardScaler(copy=True, with_mean=True, with_std=True)
-images_train_scale=scaler.fit_transform(images_train)
-images_test_scale=scaler.transform(images_test)
-#train for a bit
-# fit the model
-
-
-
-n_epochs=10000
-mini=100
-eta0=0.01
-sgd = SGDClassifier(loss="hinge", alpha=0.01, n_iter=1, fit_intercept=True,
-                    learning_rate='constant',eta0=eta0,warm_start=True,random_state=rng)
-                    
-E=pd.DataFrame(np.zeros((n_epochs,3)),columns=['part','all','test'])
-wts=np.zeros((ntrain+1,nrows*ncols+1))
-grad=np.zeros((ntrain+1,nrows*ncols+1))
-mn_grad=np.zeros((n_epochs,nrows*ncols+1))
-st_grad=np.zeros((n_epochs,nrows*ncols+1))
-
-for i in range(n_epochs):
-    #  train for a bit
-    ind=rng.randint(0,ntrain,mini)
-    sgd.partial_fit(images_train[ind,:], labels_train[ind],[0,1])
-        # then set     
-    eta0=sgd.eta0
-    sgd.eta0=1e-6
-    wts[0,:-1]=sgd.coef_
-    wts[0,-1]=sgd.intercept_
-    for im in range(ntrain):
-    
-        sgd.partial_fit(images_train[im,:].reshape((1,-1)), labels_train[im].reshape((1,)),[0,1])
-        wts[im,:-1]=sgd.coef_
-        wts[im,-1]=sgd.intercept_
-    grad=np.diff(wts,axis=0)
-    mn=grad.mean(axis=0)*1e-6
-    st=grad.std(axis=0)*1e-6
-    ind=rng.randint(0,ntrain,mini)
-    mn_grad[i,:]=mn
-    st_grad[i,:]=st
-    E["part"][i]=sgd.score(images_train_scale[ind,:], labels_train[ind])
-    E["all"][i]=sgd.score(images_train_scale, labels_train)
-    E["test"][i]=sgd.score(images_test_scale, labels_test)
-    
-    sgd.eta0=eta0
+## split into train and test set
+#
+#images_all=np.zeros((nimages,nrows*ncols))
+#images_all[:]=images[:]
+#labels_all=labels8
+#
+#
+## split into train and test set
+#rng=np.random.RandomState(2)
+#images_train,images_test, labels_train,labels_test = train_test_split(images_all, labels_all,test_size=0.33, random_state=rng)
+#ntrain=images_train.shape[0]
+#
+#images_train_mean=images_train.mean()
+#images_train_std=images_train.std()
+#
+## rescale
+#scaler=StandardScaler(copy=True, with_mean=True, with_std=True)
+#images_train_scale=scaler.fit_transform(images_train)
+#images_test_scale=scaler.transform(images_test)
--- source/mnist_chaco.py
+++ source/mnist_chaco.py
@@ -1,131 +1,164 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 30 23:15:09 2014
+Created on Tue May 06 23:40:45 2014
 
-@author: sean
+@author: sv507
 """
 
+"""
+Visualization of simulated live data stream
+
+Shows how Chaco and Traits can be used to easily build a data
+acquisition and visualization system.
+
+Two frames are opened: one has the plot and allows configuration of
+various plot properties, and one which simulates controls for the hardware
+device from which the data is being acquired; in this case, it is a mockup
+random number generator whose mean and standard deviation can be controlled
+by the user.
+"""
+
+# Major library imports
 import numpy as np
-import matplotlib.pyplot  as plt
-import pandas as pd
-from sklearn.cross_validation import train_test_split
-from sklearn.linear_model import SGDClassifier
-from sklearn.preprocessing import StandardScaler
-from traits.api import HasTraits, Float, Integer
 
-# label
+# Enthought imports
+from traits.api import (Array, Callable, Enum, Float, HasTraits, Instance, Int,
+                        Trait)
+from traitsui.api import Group, HGroup, Item, View, spring, Handler
+import mnist
 
-#[offset] [type]          [value]          [description]
-#0000     32 bit integer  0x00000801(2049) magic number (MSB first)
-#0004     32 bit integer  60000            number of items
-#0008     unsigned byte   ??               label
-#0009     unsigned byte   ??               label
-#........
-#xxxx     unsigned byte   ??               label
+# Chaco imports
+from chaco.chaco_plot_editor import ChacoPlotItem
 
 
-with open('train-labels-idx1-ubyte','rb') as f:
-    magic = np.fromfile(f,'>u4',1)[0]
-    nlabels = np.fromfile(f,'>u4',1)[0]
-    labels = np.fromfile(f,np.ubyte)
-print magic, nlabels
+class Viewer(HasTraits):
+    """ This class just contains the two data arrays that will be updated
+    by the Controller.  The visualization/editor for this class is a
+    Chaco plot.
+    """
+    index = Array
+
+    data = Array
+
+    #plot_type = Enum("line", "scatter")
+
+    view = View(ChacoPlotItem("index", "data",
+                              type_trait="plot_type",
+                              resizable=True,
+                              x_label="Time",
+                              y_label="Classification error",
+                              color="blue",
+                              bgcolor="white",
+                              border_visible=True,
+                              border_width=1,
+                              padding_bg_color="lightgray",
+                              width=800,
+                              height=380,
+                              marker_size=2,
+                              show_label=False),
+                HGroup(spring, Item("plot_type", style='custom'), spring),
+                resizable = True,
+                buttons = ["OK"],
+                width=800, height=500)
 
 
-#TRAINING SET IMAGE FILE (train-images-idx3-ubyte):
-#
-#[offset] [type]          [value]          [description]
-#0000     32 bit integer  0x00000803(2051) magic number
-#0004     32 bit integer  60000            number of images
-#0008     32 bit integer  28               number of rows
-#0012     32 bit integer  28               number of columns
-#0016     unsigned byte   ??               pixel
-#0017     unsigned byte   ??               pixel
-#........
-#xxxx     unsigned byte   ??               pixel
-# Pixels are organized row-wise. Pixel values are 0 to 255.
-# 0 means background (white), 255 means foreground (black).
+class Controller(HasTraits):
 
-with open('train-images-idx3-ubyte', 'rb') as f:
-    magic = np.fromfile(f, '>u4', 1)[0]
-    nimages = np.fromfile(f, '>u4', 1)[0]
-    nrows = np.fromfile(f, '>u4', 1)[0]
-    ncols = np.fromfile(f, '>u4', 1)[0]
-    images = np.fromfile(f, np.ubyte).reshape((nimages, nrows*ncols))
-print magic, nimages, nrows, ncols
+    # A reference to the plot viewer object
+    viewer = Instance(Viewer)
 
+    # Some parameters controller the random signal that will be generated
+    learning_rate = Enum('optimal', 'constant', 'invscaling')
+    eta0 = Float(0.01)
+    power_t = Float(0.5)
+    alpha = Float(1.0)
+    nsamples = Int(1000)
 
-class SGDLearner():
+    # The max number of data points to accumulate and show in the plot
+    #max_num_points = Int(100)
 
-    def __init__(self, X_train, y_train, X_test, y_test,
-                 random_state, eta0, alpha, T, update_period):
-        self.eta0 = eta0
-        self.alpha = alpha0
+    # The number of data points we have received; we need to keep track of
+    # this in order to generate the correct x axis data series.
+    #num_ticks = Int(0)
 
-        self.T = T
-        self.update_period = update_period
+    # private reference to the random number generator.  this syntax
+    # just means that self._generator should be initialized to
+    # random.normal, which is a random number function, and in the future
+    # it can be set to any callable object.
+    #_generator = Trait(SGDLearner, Callable)
+    def __init__(self, sgd, *args, **kw):
+        super(Controller, self).__init__(*args, **kw)
+        self.sgd=sgd
+        
+    view = View(Group('learning_rate',
+                      'eta0',
+                      'power_t',
+                      'alpha',
+                      'nsamples',
+                      orientation="vertical"),
+                      buttons=["OK", "Cancel"])
 
+    def learn_pressed(self, *args):
+        """
+        Callback function that should get called based on a timer tick.  This
+        will generate a new random data point and set it on the `.data` array
+        of our viewer object.
+        """
+        # Generate a new number and increment the tick count
+        self.sgd.learn(self.nsamples)
 
-        self.X_train = X_train
-        self.X_train = X_train
-        self.X_test = X_test
-        self.y_test = y_test
-        self.rng = random_state
-        #E_part = np.NaN((self.T // self.update_period, ))
+        # grab the existing data, truncate it, and append the new point.
+        # This isn't the most efficient thing in the world but it works.
+        
 
-        self.ntrain, self.ncoef = self.X_train.shape
+        self.viewer.index = map(lambda x:x['timestep'],self.sgd.scores)
+        self.viewer.data = map(lambda x:x['test'],self.sgd.scores)
+        return
 
-        self.wts = np.zeros((ntrain + 1, self.ncoef + 1))
-        self.grad = np.zeros((self.ntrain + 1, self.ncoef + 1))
-
-    def reset(self):
-        self.samples = pd.DataFrame(
-            {'part': 0, 'train': 0, 'test': 0},
-            index={'timesteps': np.arange(0, self.T, self.update_period)})
-        #turn into dataframe?
-        self.mn_grad = np.zeros((self.T // self.update_period, self.ncoef + 1))
-        self.st_grad = np.zeros((self.T // self.update_period, self.ncoef + 1))
-
-        self._iT = 0
-        self.sgd = SGDClassifier(warm_start=True, warm_start=True,
-                                 random_state=self.rng, fit_intercept=True)
-        self.sgd.loss = "hinge"
-        self.sgd.alpha = self.alpha
-        self.sgd.learning_rate = 'constant'
+    def _learning_rate_changed(self):
+        # This listens for a change in the learning rate.
+        self.sgd.learning_rate = self.learning_rate
+        
+    def _eta0_changed(self):
+        # This listens for a change in the learning rate.
         self.sgd.eta0 = self.eta0
+    
+    def _power_t_changed(self):
+        # This listens for a change in the learning rate.
+        self.sgd.power_t = self.power_t
+    def _alpha_t_changed(self):
+        # This listens for a change in the learning rate.
+        self.sgd.alpha = self.alpha
 
-    def learn(self):
-        '''Train for update_period and store results'''
-        ind = self.rng.randint(0, self.ntrain, self.update_period)
-        self.sgd.partial_fit(self.X_train[ind, :], self.y_train[ind], [0, 1])
 
-        self.calc_grad()
-        mn = self.grad.mean(axis=0) * 1e-6
-        st = self.grad.std(axis=0)*1e-6
+class Demo(HasTraits):
+    controller = Instance(Controller)
+    viewer = Instance(Viewer, ())
+    view = View(Item('controller', style='custom', show_label=False),
+                Item('viewer', style='custom', show_label=False),
+                handler=DemoHandler,
+                resizable=True)
 
-        self.mn_grad[self._iT // self.update_period, :] = mn
-        self.st_grad[self._iT // self.update_period, :] = st
-        self._iT += self.update_period
+    def edit_traits(self, *args, **kws):
+        # Start up the timer! We should do this only when the demo actually
+        # starts and not when the demo object is created.
+        self.timer=Timer(100, self.controller.timer_tick)
+        return super(Demo, self).edit_traits(*args, **kws)
 
-        ind = self.rng.randint(0, self.ntrain, self.ntrain*0.1)
-        self.samples[_iT, 'part'] = sgd.score(self.X_train[ind, :],
-                                              self.y_train[ind])
-        self.samples[_iT, 'train'] = sgd.score(self.X_train, self.y_train)
-        self.samples[_iT, 'test'] = sgd.score(self.X_test, self.y_test)
-        # restore orignal learning rate
-        self.sgd.eta0 = eta0
+    def configure_traits(self, *args, **kws):
+        # Start up the timer! We should do this only when the demo actually
+        # starts and not when the demo object is created.
+        self.timer=Timer(100, self.controller.timer_tick)
+        return super(Demo, self).configure_traits(*args, **kws)
 
-    def calc_grad():
-        # now calculate current gradient variance
-        # by using very low learning rate and calculate gradients
-        eta0 = self.sgd.eta0
-        self.sgd.eta0 = 1e-6*eta0
-        self.wts[0, :-1] = sgd.coef_
-        self.wts[0, -1] = sgd.intercept_
-        for im in range(self.ntrain):
+    def _controller_default(self):
+        return Controller(viewer=self.viewer)
 
-            self.sgd.partial_fit(self.X_train[im, :].reshape((1, -1)),
-                                 self.y_train[im].reshape((1, )), [0, 1])
-            self.wts[im, :-1] = self.sgd.coef_
-            self.wts[im, -1] = self.sgd.intercept_
 
-        self.grad = np.diff(self.wts, axis=0)
+# NOTE: examples/demo/demo.py looks for a 'demo' or 'popup' or 'modal popup'
+# keyword when it executes this file, and displays a view for it.
+popup=Demo()
+
+
+if __name__ == "__main__":
+    popup.configure_traits()
