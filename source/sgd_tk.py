80cfa61156162113834b6e0cb246d6f2c073b207
Sean Violante <sean.violante@gmail.com>
2014-06-10 23:37:32 +0200

    in progress charts... not connected to learner

--- source/sgd_tk.py
+++ source/sgd_tk.py
@@ -8,7 +8,10 @@ from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolb
 # implement the default mpl key bindings
 from matplotlib.backend_bases import key_press_handler
 
-
+#TODO
+    # when press learn, take strings and check f numbers have changed.
+   # if so mark  on timeline
+#
 from matplotlib.figure import Figure
 
 import sys
@@ -20,79 +23,57 @@ else:
 root = Tk.Tk()
 root.wm_title("SGD Demo")
 
-# main frame
-fr_learn = Tk.Frame(root)
+class Learn_Chart:
+    def __init__(self, root):
+        self.root=root
 
-# figure
-f = plt.Figure(figsize=(5,4), dpi=100)
-a = f.add_subplot(111)
+        # main frame
+        self.fr_learn = Tk.Frame(root)
 
-# a tk.DrawingArea
-canvas = FigureCanvasTkAgg(f, master=fr_learn)
-canvas.show()
-canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+        # figure
+        f = plt.Figure(figsize=(5,4), dpi=100)
+        a = f.add_subplot(111)
 
-variables=[]
-labels=[]
+    # a tk.DrawingArea
+        self.canvas = FigureCanvasTkAgg(f, master=self.fr_learn)
+        self.canvas.show()
+        self.canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+        self.canvas._tkcanvas.pack(side=Tk.RIGHT, expand=1, fill=Tk.BOTH )
 
-fr_buttons=Tk.Frame(fr_learn)
+        self.fr_buttons=Tk.Frame(self.fr_learn)
+        self.labels={}
+        self.vars={}
+        var_list=[('alpha',0.01),
+                  ('eta0',  0.1),
+                  ('probe every', 1000),
+                  ('learn for', 10000) ]
+        for var, default in var_list:
+            self.vars[var]=Tk.StringVar()
+            self.vars[var].set(default)
 
-var = Tk.StringVar()
-var.set('0.01')
-variables.append(var)
+            fr_row = Tk.Frame(self.fr_buttons)
 
-fr_row = Tk.Frame(fr_buttons)
+            self.labels[var]= Tk.Label(fr_row, text=var)
+            self.labels[var].pack(side=Tk.LEFT)
+            self.labels[var].config(width=10)
+            #label_width_max=max(map(lambda x: x.config('width'),labels))
+            Tk.Entry(fr_row, textvariable=self.vars[var]).pack(side=Tk.RIGHT, expand=Tk.YES, fill=Tk.X)
+            fr_row.pack(side=Tk.TOP)
 
-labels.append( Tk.Label(fr_row, text='alpha'))
-labels[-1].pack(side=Tk.LEFT)
-Tk.Entry(fr_row, textvariable=var).pack(side=Tk.RIGHT, expand=Tk.YES, fill=Tk.X)
-fr_row.pack(side=Tk.TOP)
 
-var = Tk.StringVar()
-var.set('0.02')
-variables.append(var)
-fr_row = Tk.Frame(fr_buttons)
+        Tk.Button(self.fr_buttons,text='reset').pack(side=Tk.TOP)
+        Tk.Button(self.fr_buttons,text='learn').pack(side=Tk.TOP)
 
-labels.append(Tk.Label(fr_row, text='eta0'))
+        self.fr_buttons.pack(side=Tk.LEFT)
+        self.fr_learn.pack(side=Tk.TOP,expand=1,fill=Tk.BOTH)
 
-labels[-1].pack(side=Tk.LEFT)
-Tk.Entry(fr_row, textvariable=var).pack(side=Tk.RIGHT, expand=Tk.YES, fill=Tk.X)
-fr_row.pack(side=Tk.TOP)
+        self.toolbar = NavigationToolbar2TkAgg( self.canvas, self.root )
+        self.toolbar.update()
 
-Tk.Button(fr_buttons,text='reset').pack(side=Tk.TOP)
 
-fr_row=Tk.Frame(fr_buttons)
-labels.append(Tk.Label(fr_row, text='probe every'))
-labels[-1].pack(side=Tk.LEFT)
-Tk.Entry(fr_row).pack(side=Tk.RIGHT, expand=Tk.YES, fill=Tk.X)
-fr_row.pack(side=Tk.TOP)
 
-fr_row = Tk.Frame(fr_buttons)
-labels.append(Tk.Label(fr_row, text='learn for'))
-labels[-1].pack(side=Tk.LEFT)
-Tk.Entry(fr_row).pack(side=Tk.RIGHT, expand=Tk.YES, fill=Tk.X)
-fr_row.pack(side=Tk.TOP)
 
-#label_width_max=max(map(lambda x: x.config('width'),labels))
-for l in labels:
-    l.config(width=10)
 
-Tk.Button(fr_buttons,text='learn').pack(side=Tk.TOP)
-
-fr_buttons.pack(side=Tk.LEFT)
-
-toolbar = NavigationToolbar2TkAgg( canvas, root )
-toolbar.update()
-
-canvas._tkcanvas.pack(side=Tk.RIGHT, expand=1, fill=Tk.BOTH )
-
-fr_learn.pack(side=Tk.TOP,expand=1,fill=Tk.BOTH)
-
-def on_key_event(event):
-    print('you pressed %s'%event.key)
-    key_press_handler(event, canvas, toolbar)
-
-canvas.mpl_connect('key_press_event', on_key_event)
 
 def _quit():
     root.quit()     # stops mainloop
@@ -101,6 +82,8 @@ def _quit():
 
 #button = Tk.Button(master=fr_learn, text='Quit', command=_quit)
 #button.pack(side=Tk.BOTTOM)
+
+a=Learn_Chart(root)
 
 Tk.mainloop()
 # If you put root.destroy() here, it will cause an error if
