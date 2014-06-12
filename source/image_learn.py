a136beeb6c39e63cb27917c49235e9fb872dcd52
Sean Violante <sean.violante@gmail.com>
2014-06-12 18:55:25 +0200

    under dev

--- source/image_learn.py
+++ source/image_learn.py
@@ -35,8 +35,6 @@ def unpickle(file):
 
 def load_cifar10():
     for  batch in range(1,2):     
-    
-    
         file_name='/Users/sean/projects/data/cifar10/cifar-10-batches-py/data_batch_{0}'.format(batch)    
         cifars=unpickle(file_name)    
         images=cifars['data']
@@ -44,11 +42,15 @@ def load_cifar10():
 
 
 class ImageTransform:
-    def __init__(n_channels=3, height=32,width=32):
-    
+    def __init__(self, n_channels=3, height=32,width=32):
+        self.n_channels=n_channels
+        self.height=height
+        self.width=width
+        
     def get_params(self, deep=True):
     # suppose this estimator has parameters "alpha" and "recursive"
-        return ImageTranform.get_params(self, deep=True)
+        return {'n_channels': self.n_channels, 'height' : self.height, 
+        'width' : self.width}
 
     def set_params(self, **parameters):
         for parameter, value in parameters.items():
@@ -61,7 +63,7 @@ class SIFTTransform( ImageTransform):
     def get_params(self, deep=True):
     # suppose this estimator has parameters "alpha" and "recursive"
         
-        return ImageTranform.get_params(self, deep=True)
+        return ImageTransform.get_params(self, deep=True)
 
     def set_params(self, **parameters):
         for parameter, value in parameters.items():
@@ -85,7 +87,7 @@ class SIFTTransform( ImageTransform):
             'angle':np.float, 'response':np.float,
             'octave':np.int, 'class_id':np.int}
             
-            pd.DataFrame(zeros((len(kps),6),fields)
+            pd.DataFrame(zeros((len(kps),6),fields))
                 
             for kp in kps:
                 x,y =kp.pt
@@ -101,23 +103,24 @@ class SIFTTransform( ImageTransform):
 # yaml script
 
 
-class ConvolveFit:
+class ConvolveTransform (ImageTransform):
     # defaults necc for automation
-    def __init__(self, channels=3,height=32,width=32, filter_height=8,filter_width=8,stride=3,filt=Kmeans(), post_filter='average'):
-        self.filter=filt
+    def __init__(self, channels=3,height=32,width=32, filter_height=8,filter_width=8,stride=2):
+
         self.filter_height=filter_height
         self.filter_width=filter_width
+        self.stride=stride
+        ImageTransform.__init__(self, channels, height, width)
         
-        self.post_filter=post_filter
-    def fit(X,y=None):
+    def generate_filter_data(self, X):        
         """ 
         generate subimages (so number of samples ie rows increases)
         apply kmeans/sparse pca...
         store feature vectors
         
         """        
-        stride_x=range(0,self.width-self.sub_width,stride)
-        stride_y=range(0,self.height-self.sub_height,stride)
+        strides_x=range(0,self.width-self.filter_width,self.stride)
+        strides_y=range(0,self.height-self.filter_height,self.stride)
         sub_images=[]
         for image in X:
             img_rgb=image.reshape((self.n_channels,self.height,self.width))
@@ -129,17 +132,69 @@ class ConvolveFit:
                     sub_images.append(sub_image)
            
            # how to avoid duplicating data? and write to files, also so diff filters can be used           
-           all_sub_images=np.vstack(sub_images)             
-           self.filter.fit(all_sub_images)
-           # how to avoid duplicating data
-        return self
-        
+        all_sub_images=np.vstack(sub_images)
+        return all_sub_images
+         
     def transform(self,X):
         """ convert each single image into features
         """
-    def fit_transform(X,y):
-        """ first create dictionary, then convert each single image into features
-        """
+def gcn(X,scale=1., subtract_mean=True, use_std=False,
+                              sqrt_bias=0., min_divisor=1e-8):
+    mean = X.mean(axis=1)
+    if subtract_mean:
+        X = X - mean[:, np.newaxis]  # Makes a copy.
+    else:
+        X = X.copy()    
+         # how to deal with colour!!!
+    if use_std:
+        # ddof=1 simulates MATLAB's var() behaviour, which is what Adam
+        # Coates' code does.
+        ddof = 1
+
+        # If we don't do this, X.var will return nan.
+        if X.shape[1] == 1:
+            ddof = 0
+
+        normalizers = np.sqrt(sqrt_bias + X.var(axis=1, ddof=ddof)) / scale
+    else:
+        normalizers = np.sqrt(sqrt_bias + (X ** 2).sum(axis=1)) / scale
+
+    # Don't normalize by anything too small.
+    normalizers[normalizers < min_divisor] = 1.
+
+    X /= normalizers[:, np.newaxis]  # Does not make a copy.
+    return X
+
+def gcn_covar(X,n_channels=3, scale=1., subtract_mean=True, use_std=False,
+                              sqrt_bias=0., min_divisor=1e-8):
+    n_pixels=X.shape[1]/n_channels
+    X=X.reshape((X.shape[0],n_channels,-1))
+    mean = X.mean(axis=2)
+    if subtract_mean:
+        X = X - mean[:,:, np.newaxis]  # Makes a copy.
+    else:
+        X = X.copy()    
+         # how to deal with colour!!!
+    if use_std:
+        # ddof=1 simulates MATLAB's var() behaviour, which is what Adam
+        # Coates' code does.
+        ddof = 1
+
+        # If we don't do this, X.var will return nan.
+        if n_pixels == 1:
+            ddof = 0
+
+        normalizers = np.sqrt(sqrt_bias + X.var(axis=2, ddof=ddof)) / scale
+    else:
+        normalizers = np.sqrt(sqrt_bias + (X ** 2).sum(axis=2)) / scale
+
+    # Don't normalize by anything too small.
+    normalizers[normalizers < min_divisor] = 1.
+
+    X /= normalizers[:, :, np.newaxis]  # Does not make a copy.
+    return X
+
+
     
 def preprocess(X, n_components=50):
     ss=StandardScaler()
@@ -148,19 +203,37 @@ def preprocess(X, n_components=50):
     pip=Pipeline([('standardscaler',ss),('pCA', pca)])
     X_transform=pip.fit_transform(X)
     return X_transform,pip
+
+def preproc_coates(patches):
+    ''' first contrast enhance - remove image mean from each image [what about colour]      
+        divide by stdev
+        so each image has mean zero, stdev 1 across pixels, 
+        then sphere ie apply pca and whiten ( ie all components )
+    '''
+    gcn_patches=gcn(patches,scale=1., subtract_mean=True, use_std=True,
+                              sqrt_bias=10., min_divisor=1e-8)
+
+    pca = PCA(n_components=X.shape[1], whiten=True)
+    pca_gcn_patches=pca.fit_transform(gcn_patches)
+    return pca_gcn_patches,pca
+    
+    
     
 #img=cv2.drawKeypoints(gray,kp)
 
 #cv2.imwrite('sift_keypoints.png',img)
 #cv2.imwrite('img.png',img_bgr)
 
-X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.33)
-n_train=X_train.shape[0]
-kf=KFold(n=n_train, n_folds=10 )
+#X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.33)
+#n_train=X_train.shape[0]
+#kf=KFold(n=n_train, n_folds=10 )
 
-log_r = LogisticRegression()
+#log_r = LogisticRegression()
 
-gs=GridSearchCV(log_r, param_grid={'C':[0.01,0.1,1,10,100]}, cv=kf)
+#gs=GridSearchCV(log_r, param_grid={'C':[0.01,0.1,1,10,100]}, cv=kf)
 
+# generate sub image data for diff sizes (ideally random sampling position)
+# do kmeans
+# recover 
 
 
